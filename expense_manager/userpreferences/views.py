806c9cd5df1da05af78ead93fceb6c5fd6d41a3e
hemanthhvv <vvhemanthh@gmail.com>
2024-10-10 13:36:00 +0530

    Userpreferences app added

---
+++ expense_manager/userpreferences/views.py
@@ -0,0 +1,34 @@
+from asyncio.log import logger
+from django.shortcuts import render
+import json,os
+from django.conf import settings
+from .models import UserPreference
+from django.contrib import messages
+# Create your views here.
+
+def index(request):
+    exists = UserPreference.objects.filter(user=request.user).exists()
+    # userpreference = None
+    if exists:
+        userpreference = UserPreference.objects.get(user=request.user)
+    currency_data = []
+    file = os.path.join(settings.BASE_DIR,"currencies.json")
+    with open(file=file,mode="r") as currencies:
+        currencies = json.load(currencies)
+    for curr,coun in currencies.items():
+        currency_data.append({'name':curr,'country':coun})
+
+    if request.method == "GET":
+        return render(request,'preferences/index.html',{'currencies':currency_data,'userpreference':userpreference})
+
+    else:
+        currency_selected = request.POST['currency']
+        if exists:
+            userpreference.currency = currency_selected
+            userpreference.save()
+        else:
+            UserPreference.objects.create(user=request.user,currency=currency_selected)
+        messages.success(request,"Currency saved Successfully.")
+        print("=================> ",str(currency_selected))
+        # print("=================> ",currency_selected['curr'])
+        return render(request,'preferences/index.html',{'currencies':currency_data,'userpreference':userpreference})
