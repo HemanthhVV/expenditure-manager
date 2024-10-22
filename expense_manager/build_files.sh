488e1439f7ffbe984346e802d83f915bbd2fcf57
hemanthhvv <vvhemanthh@gmail.com>
2024-10-22 19:51:05 +0530

    minor changes

--- expense_manager/build_files.sh
+++ expense_manager/build_files.sh
@@ -2,10 +2,13 @@
 echo "Starting...."
 mkdir venv
 echo "venv created"
-python -m venv ./venv
-echo "activated"
 pwd
-ls venv
+echo "printing dir"
+ls
+echo "done printing dir"
+python -m venv venv/
+echo "activated"
+ls venv/
 source venv/bin/activate
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
