8e7b90532bab3f911fb0d6338f538ec3e9be76f1
hemanthhvv <vvhemanthh@gmail.com>
2024-10-22 19:42:54 +0530

    minor changes

--- expense_manager/build_files.sh
+++ expense_manager/build_files.sh
@@ -1,7 +1,11 @@
 # build_files.sh
 echo "Geeks for Geeks"
 mkdir venv
+echo "venv created"
 python -m venv ./venv
+echo "activated"
+pwd
+ls
 source ./venv/bin/activate
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
