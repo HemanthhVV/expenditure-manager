249ae15569d51d1af563bebbc5adb2d5dc49cb57
hemanthhvv <vvhemanthh@gmail.com>
2024-10-22 21:37:57 +0530

    debugging deployment

--- expense_manager/expense_manager/settings.py
+++ expense_manager/expense_manager/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'expense_manager/expense_manager/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
