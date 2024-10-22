67341ef5d1d0055c49493bb28cd89ad366b828e8
hemanthhvv <vvhemanthh@gmail.com>
2024-10-22 20:44:42 +0530

    debugging deployment

--- expense_manager/expense_manager/settings.py
+++ expense_manager/expense_manager/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT=os.path.join(BASE_DIR,'static/')
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'staticfiles_build','static')
+    os.path.join(BASE_DIR,'expense_manager','static')
     ]
 
 
